4df3ded0022b95efcc6ac135764877826e581043
Kacie Cooper <kcrouch@pdx.edu>
2020-01-23 19:45:06 -0800

    trying

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,34 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   
 
+$(document).ready(function(){
+  // Change this to the correct selector.
+  $('nav.fixed').fixed();
+});
 
 
+$(document).ready(function(){
+	$(window).scroll(function(){
+  	var light_pos = $('#white_div').offset().top;
+    var light_height = $('#white_div').height();
+    var menu_pos = $('.NavigationButton').offset().top;
+    var menu_height = $('.NavigationButton').height();
+    var scroll = $(window).scrollTop();
+    console.log('light',light_pos);
+    console.log('menu',menu_pos);
+    console.log('scroll',scroll);
+    
+    if(menu_pos > light_pos && menu_pos < (light_pos + light_height)) {
+    	$('.NavigationButton').addClass('menu_black');
+      $('.NavigationButton').removeClass('menu_white');
+    }
+    else {
+    	$('.NavigationButton').removeClass('menu_black');
+      $('.NavigationButton').addClass('menu_white');
+    }
 
 
-
-
-
-
-
+//-----------------------
 
 
 
