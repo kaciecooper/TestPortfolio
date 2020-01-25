41f62a9bff96c8061b0bd318730b5bd08ee3e55e
Kacie Cooper <kcrouch@pdx.edu>
2020-01-24 18:00:46 -0800

    landing page finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,20 +3,16 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   
 
-$(document).ready(function(){
-  // Change this to the correct selector.
-  $('nav.fixed').fixed();
-});
 
 
 $(window).scroll(function() {
                 var top = $(this).scrollTop();
                 if (top > 57) {
-                   $(".perspective").css("color", "black");
+                   $(".quote").css("color", "black");
 
                 } 
                else {
-                   $(".perspective").css("color", "white");
+                   $(".quotebox-2").css("color", "white");
                  
 
 
@@ -25,32 +21,9 @@ $(window).scroll(function() {
  })
 
 
-var navTop = $('#perspective').offset().top;
-var navStop = $('#stop').offset().top;
-var lastMode = "absolute";
 
-$(window).scroll(function() {
-  var mode;
-  if ($(this).scrollTop() >= navTop) {
-    if ($(this).scrollTop() - navStop + $('#perspective').height() > 0)
-      mode = 'absolute';
-    else
-      mode = 'fixed';
-  } else {
-    mode = 'absolute';
-  }
 
-  if (lastMode !== mode) {
-    if (mode == 'fixed') {
-      $('#perspective').css('position', 'fixed');
-      $('#perspective').css('top', '0');
-    } else {
-      $('#perspective').css('position', 'absolute');
-      $('#perspective').css('top', navTop);
-    }
-    lastMode = mode;
-  }
-});
+	
 
 //-----------------------
 
