a6c359d71701c583c6d1b12f276dde28b49223ba
Kacie Cooper <kcrouch@pdx.edu>
2020-01-26 10:59:26 -0800

    tying

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,11 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
 
+
+new fullpage('#fullpage', {
+  licenseKey: 'YOUR KEY HERE',
+  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+});
 // End of Your Code . Don't delete that line below!!
 });
