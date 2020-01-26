4799911e121058f1ef2c9aa3fc021fc8d4003ebb
Kacie Cooper <kcrouch@pdx.edu>
2020-01-26 11:22:52 -0800

    trying

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,9 +3,7 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
 
 
-new fullpage('#fullpage', {
-  licenseKey: 'YOUR KEY HERE',
-  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-});
+
+
 // End of Your Code . Don't delete that line below!!
 });
