b41635568dcdf627093df0629f15f36c957f8dae
Kacie Cooper <kcrouch@pdx.edu>
2020-01-25 21:37:27 -0800

    trying

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,16 +3,5 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   
 
-
-
-
-
-	
-
-//-----------------------
-
-
-
-
 // End of Your Code . Don't delete that line below!!
 });
