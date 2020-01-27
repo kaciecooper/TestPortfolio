9a7b063930885b3c6ab8d854d53aa5380c3d6b66
Kacie Cooper <kcrouch@pdx.edu>
2020-01-26 17:19:31 -0800

    added animations to homepage

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
 
-
+////////////////////////////////////////////////////////////Page 1/3 Script
 $(document).ready(function(){
 
 	//Init ScrollMagin
@@ -18,23 +18,72 @@ $(document).ready(function(){
 	})
 
 	.setClassToggle('.question', 'fade-in') //add class to project01
-	.addIndicators({
-		name:'fade scene',
-		colorTrigger:'black',
-	})
+	
 	.addTo(controller)
 });
-
+////////////////////////////////////////////////////////////Page 1/2 Script
 var controller = new ScrollMagic.Controller();
 
 $(function () { // wait for document ready
 		// build scene
 		var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 850})
 						.setPin("#pin1")
-						.addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
+						
 						.addTo(controller);
 	});
-				
+
+////////////////////////////////////////////////////////////Page 2/2 Script
+
+var controller = new ScrollMagic.Controller();
+
+// build scene
+		new ScrollMagic.Scene({
+							triggerElement: "#trigger2",
+							triggerHook: 0.9, // show, when scrolled 10% into view
+							duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+							offset: 50 // move trigger to center of element
+						})
+						.setClassToggle("#reveal2", "visible") // add class to reveal
+					
+						.addTo(controller);
+
+////////////////////////////////////////////////////////////////Perspective Script
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 50) {
+    $('.rightside').hide();
+  } else {
+    $('.rightside').show();
+  }
+});
+
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 50) {
+    $('.upsidedown').show();
+  } else {
+    $('.upsidedown').hide();
+  }
+});
+
+///////////////////////////////////////////////////////////////Quote Script
+
+	var controller = new ScrollMagic.Controller();
+
+	var animateElem = document.getElementById("animate2");
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger3", duration: 400})
+					.on("enter", function () {
+						// trigger animation by changing inline style.
+						animateElem.style.color = "#fff";
+					})
+					.on("leave", function () {
+						// reset style
+						animateElem.style.backgroundColor = "";
+					})
+					.addTo(controller);
+
 
 // End of Your Code . Don't delete that line below!!
 });
