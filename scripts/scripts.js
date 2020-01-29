c9802fbc7e5c06683faca200bc1b24cfdb206eae
Kacie Cooper <kcrouch@pdx.edu>
2020-01-28 22:22:03 -0800

    uploaded svg

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -60,7 +60,7 @@ var controller = new ScrollMagic.Controller({
 			var duration = scene.duration();
 
 
-///////////////////////////////////////////////////////////////Page 1 Quote
+///////////////////////////////////////////////////////////////
 	
 
 
@@ -75,70 +75,6 @@ var controller = new ScrollMagic.Controller({
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////Page 3 Panels
-
-
-// define images
-	var images = [
-		"images/badday-img1.png",
-		"images/badday-img2.png",
-		"images/badday-img3.png",
-		"images/badday-img4.png",
-		"images/badday-img5.png",
-		"images/badday-img6.png",
-		"images/badday-img7.png",
-		"images/badday-img8.png",
-		"images/badday-img9.png",
-		"images/badday-img10.png",
-		"images/badday-img11.png",
-		"images/badday-img12.png",
-		"images/badday-img13.png",
-		"images/badday-img14.png",
-		"images/badday-img15.png"
-	];
-
-	// TweenMax can tween any property of any object. We use this object to cycle through the array
-	var obj = {curImg: 0};
-
-	// create tween
-	var tween = TweenMax.to(obj, 0.5,
-		{
-			curImg: images.length - 1,	// animate propery curImg to number of images
-			roundProps: "curImg",				// only integers so it can be used as an array index
-			repeat: 3,									// repeat 3 times
-			immediateRender: true,			// load first image automatically
-			ease: Linear.easeNone,			// show every image the same ammount of time
-			onUpdate: function () {
-			  $("#myimg").attr("src", images[obj.curImg]); // set the image source
-			}
-		}
-	);
-
-	// init controller
-	var controller = new ScrollMagic.Controller();
-
-	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 300})
-					.setTween(tween)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-
-	// handle form change
-	$("form.move input[name=duration]:radio").change(function () {
-		scene.duration($(this).val());
-	});
-
-
+///////////////////////////////////////////////////////////////
 
 });
