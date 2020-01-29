222d7180c591be3f3bcca5f9130469046cac165e
Kacie Cooper <kcrouch@pdx.edu>
2020-01-28 16:08:54 -0800

    page 3 panels set

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -63,4 +63,63 @@ var controller = new ScrollMagic.Controller({
 ///////////////////////////////////////////////////////////////Page 1 Quote
 	
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////Page 3 Panels
+
+$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave',
+				duration: "100%" // this works just fine with duration 0 as well
+				// However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+				// Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("#panel");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i], {pushFollowers: false})
+				.addIndicators({
+					name:"panel change",
+				    colorStart:"pink",
+
+				}) // add indicators (requires plugin)
+				.addTo(controller);
+		}
+	});
+
+
+
+
+
+
 });
