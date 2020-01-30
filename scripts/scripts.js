8494e15c48d556093f14e3b54bd4e907cd73a2ea
Kacie Cooper <kcrouch@pdx.edu>
2020-01-29 19:40:42 -0800

    validating html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -63,13 +63,15 @@ var controller = new ScrollMagic.Controller({
 ///////////////////////////////////////////////////////////////
 	
 
+	// init controller
+	var controller = new ScrollMagic.Controller();
 
-
-
-
-
-
-
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
+					// trigger a velocity opaticy animation
+					.setVelocity("#animate", {opacity: 0}, {duration: 400})
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
 
 
 
